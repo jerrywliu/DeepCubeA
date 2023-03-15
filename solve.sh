1e11788ea7e2337caba9fad2c1474de912fc1163
Cheng Chang <chc012@stanford.edu>
2023-03-14 21:23:16 -0700

    udpate solve.sh for result generation

---
+++ solve.sh
@@ -0,0 +1,2 @@
+
+python search_methods/astar.py --states data/cube3/test/data_0.pkl --model saved_models/cube3/current/ --env cube3 --weight 0.6 --batch_size 100 --results_dir results/cube3_intermediate_reward/ --language cpp --nnet_batch_size 100
