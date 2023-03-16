2afc88fe98483c5580496eaa9ab20324b3fd8549
Cheng Chang <chc012@stanford.edu>
2023-03-16 05:14:04 +0000

    the correct trajectory sample pictures

--- results/check_results.py
+++ results/check_results.py
@@ -10,13 +10,14 @@ from tqdm import tqdm
 
 COLOR_MAP = np.array([
     [255, 255, 255], # white
-    [0, 255, 0], # green
-    [255, 0, 0], # red
-    [0, 0, 255], # blue
-    [255, 165, 0], # orange
     [255, 255, 0], # yellow
+    [0, 0, 255], # blue
+    [0, 255, 0], # green
+    [255, 165, 0], # orange
+    [255, 0, 0], # red
 ])
 FLOWCHART_WIDTH = 8
+MAP = np.array([8, 5, 2, 7, 4, 1, 6, 3, 0, 35, 32, 29, 34, 31, 28, 33, 30, 27, 53, 50, 47, 52, 49, 46, 51, 48, 45, 26, 23, 20, 25, 22, 19, 24, 21, 18, 44, 41, 38, 43, 40, 37, 42, 39, 36, 17, 14, 11, 16, 13, 10, 15, 12, 9])
 
 
 def plot(traj, sample_id, result_path):
@@ -32,14 +33,15 @@ def plot(traj, sample_id, result_path):
         axes[0, 1].imshow(COLOR_MAP[traj[sample_id][k][0]])
         axes[1, 0].imshow(COLOR_MAP[traj[sample_id][k][1]])
         axes[1, 1].imshow(COLOR_MAP[traj[sample_id][k][2]])
-        axes[1, 2].imshow(COLOR_MAP[traj[sample_id][k][3]])
-        axes[1, 3].imshow(COLOR_MAP[traj[sample_id][k][4]])
+        axes[1, 2].imshow(COLOR_MAP[traj[sample_id][k][3]]) # WARNING: changed 
+        axes[1, 3].imshow(COLOR_MAP[traj[sample_id][k][4]]) # WARNING: changed
         axes[2, 1].imshow(COLOR_MAP[traj[sample_id][k][5]])
 
         save_dir = result_path + "sample_traj/{}/".format(sample_id)
         if not os.path.exists(save_dir):
             os.makedirs(save_dir)
         fig.savefig(save_dir + "{}.png".format(k))
+        plt.close()
 
 
 def combine_imgs(result_path, idx, traj_len):
@@ -67,6 +69,7 @@ def combine_imgs(result_path, idx, traj_len):
 
     save_path = result_path + "sample_traj/full_{}.png".format(idx)
     fig.savefig(save_path)
+    plt.close()
 
 
 
@@ -89,6 +92,8 @@ def main(result0_path, result1_path, num_samples=10):
         ])
         times.append(result["times"][:num_samples])
         num_nodes.append(result["num_nodes_generated"][:num_samples])
+        #print(trajs[-1][-1][-1])
+        #print(np.reshape(trajs[-1][-1][-1], (6, 3, 3)))
 
         # convert indexed colors to color map
         for traj in trajs[-1]:
@@ -96,18 +101,19 @@ def main(result0_path, result1_path, num_samples=10):
                 for i in range(len(colors)):
                     if colors[i] < 9: # white
                         colors[i] = 0
-                    elif colors[i] < 18: # green
+                    elif colors[i] < 18: # yellow
                         colors[i] = 1
-                    elif colors[i] < 27: # red
+                    elif colors[i] < 27: # blue
                         colors[i] = 2
-                    elif colors[i] < 36: # blue
+                    elif colors[i] < 36: # green
                         colors[i] = 3
                     elif colors[i] < 45: # orange
                         colors[i] = 4
-                    else: # yellow
+                    else: # red
                         colors[i] = 5
             for j in range(len(traj)):
-                traj[j] = np.reshape(traj[j], (6, 3, 3))
+                traj[j] = np.reshape(traj[j][MAP], (6, 3, 3))
+                #traj[j] = np.transpose(traj[j], axes=(0, 2, 1))
     
     for traj, result_path in zip(trajs, [result0_path, result1_path]):
         for sample_idx in tqdm(range(len(traj))):
